dcfe9fd80ce5606384b8629ece97cff229cf4e2f
Tollmar, Andreas <andreas.tollmar@ssab.com>
2024-05-10 09:30:07 +0200

    change how to handle request and moved a part of the code

--- Avigilon.Infrastructure/Services/TokenProvider.cs
+++ Avigilon.Infrastructure/Services/TokenProvider.cs
@@ -1,47 +1,45 @@
-﻿namespace Avigilon.Infrastructure.Services
+﻿namespace Avigilon.Infrastructure.Services;
+public class TokenProvider(HttpClientProivider clientProivider)
 {
-    public class TokenProvider(HttpClientProivider clientProivider)
+    private readonly HttpClientProivider _clientProivider = clientProivider;
+
+    public async Task<string> GenerateSessionTokenAsync(string userNonce, string userKey, string userName, string userPassword, string clientName)
     {
-        private readonly HttpClientProivider _clientProivider = clientProivider;
+        var httpClient = _clientProivider.GetHttpClient();
 
-        public async Task<string> GenerateSessionTokenAsync(string userNonce, string userKey, string userName, string userPassword, string clientName)
+        var generator = new AuthTokenGenerator(userNonce, userKey);
+        var authToken = generator.GenerateToken();
+        Uri requestUri = new Uri("https://srv03367:8443/mt/api/rest/v1/login");
+
+        var serializerSettings = new JsonSerializerOptions
         {
-            var httpClient = _clientProivider.GetHttpClient();
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false
+        };
 
-            var generator = new AuthTokenGenerator(userNonce, userKey);
-            var authToken = generator.GenerateToken();
-            Uri requestUri = new Uri("https://srv03367:8443/mt/api/rest/v1/login");
+        string bodyContent = JsonSerializer.Serialize(new LoginRequestContract
+        {
+            AuthorizationToken = authToken,
+            Username = userName,
+            Password = userPassword,
+            ClientName = clientName
+        }, serializerSettings);
 
-            var serializerSettings = new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                WriteIndented = false
-            };
-
-            string bodyContent = JsonSerializer.Serialize(new LoginRequestContract
-            {
-                AuthorizationToken = authToken,
-                Username = userName,
-                Password = userPassword,
-                ClientName = clientName
-            }, serializerSettings);
-
-            StringContent content = new StringContent(bodyContent, Encoding.UTF8, "application/json");
-            HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(requestUri, content);
-            if (!httpResponseMessage.IsSuccessStatusCode)
-            {
-                throw new Exception($"Failed to login: response status code was {httpResponseMessage.StatusCode}");
-            }
-
-            LoginResponseContract loginResponseContract = JsonSerializer.Deserialize<LoginResponseContract>(await httpResponseMessage.Content.ReadAsStringAsync(), serializerSettings);
-            if (loginResponseContract?.Status != "success")
-            {
-                throw new Exception("Failed to login: " + loginResponseContract?.Status);
-            }
-
-            var session = loginResponseContract.Result.Session;
-            return session;
+        StringContent content = new StringContent(bodyContent, Encoding.UTF8, "application/json");
+        HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(requestUri, content);
+        if (!httpResponseMessage.IsSuccessStatusCode)
+        {
+            throw new Exception($"Failed to login: response status code was {httpResponseMessage.StatusCode}");
         }
 
+        LoginResponseContract loginResponseContract = JsonSerializer.Deserialize<LoginResponseContract>(await httpResponseMessage.Content.ReadAsStringAsync(), serializerSettings);
+        if (loginResponseContract?.Status != "success")
+        {
+            throw new Exception("Failed to login: " + loginResponseContract?.Status);
+        }
+
+        var session = loginResponseContract.Result.Session;
+        return session;
     }
+
 }
--- AvigilonApi/Program.cs
+++ AvigilonApi/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<HttpClientProivider>();
 builder.Services.AddScoped<TokenProvider>();
+builder.Services.AddScoped<HandleMediaResponse>();
 
 
 
