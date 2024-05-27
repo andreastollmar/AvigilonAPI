5c1ff40f6182c1c26fe9580eda6a1912ea059fa0
Tollmar, Andreas <andreas.tollmar@ssab.com>
2024-05-27 08:33:33 +0200

    cleanup unused global using

--- AvigilonApi/GlobalUsing.cs
+++ AvigilonApi/GlobalUsing.cs
@@ -1,9 +1,7 @@
 ﻿global using Gralin.Avigilon.ControlCenterAPI;
 global using Gralin.Avigilon.ControlCenterAPI.DataContracts;
 global using Microsoft.AspNetCore.Mvc;
-global using System.Net.Http;
-global using System.Text.Json;
-global using System.IO;
 global using Avigilon.Core.Models;
 global using Avigilon.Core.Interfaces;
 global using Avigilon.Infrastructure.Services;
+global using Avigilon.Infrastructure.Validation;
