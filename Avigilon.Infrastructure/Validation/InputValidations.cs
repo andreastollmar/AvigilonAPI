acd1a53f0e57ba671d76dec3ed5c35f14be9dded
Tollmar, Andreas <andreas.tollmar@ssab.com>
2024-05-27 08:15:05 +0200

    added testProject and some tests for validation

--- Avigilon.Infrastructure/Validation/InputValidations.cs
+++ Avigilon.Infrastructure/Validation/InputValidations.cs
@@ -8,7 +8,7 @@ public class InputValidations
         var pattern = "^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[1][0-9]|2[0-9]|3[01])$";
         var regex = new Regex(pattern);
         var match = regex.Match(date);
-        if (match != null) 
+        if (match.Success) 
         {
             return true;
         }
